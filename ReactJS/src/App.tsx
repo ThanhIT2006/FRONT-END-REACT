c7e27ef2a42ff051ed3ea30539b93ee8dab79840
ThanhIT2006 <nguyenngocthanh15122006@gmail.com>
2025-08-19 15:00:16 +0700

    BT

---
+++ ReactJS/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  const textColor = "red";
+  return (
+    <>
+     <h1 style={{ color: textColor }}>DEMO</h1>
+     <p style={{ color: textColor }}>MMBEO</p>
+    </>
+  )
+}
+
+export default App
