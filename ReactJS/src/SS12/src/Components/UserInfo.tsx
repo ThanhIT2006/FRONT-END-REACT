c300b72842e40ffaefbb7cc8d4731155cf986b76
ThanhIT2006 <nguyenngocthanh15122006@gmail.com>
2025-08-20 15:54:43 +0700

    BT

--- ReactJS/src/SS12/src/Components/UserInfo.tsx
+++ ReactJS/src/SS12/src/Components/UserInfo.tsx
@@ -1,7 +1,7 @@
 function UserInfo() {
     const user = {
-        Name: "Nguyễn Văn A",
-        sex: "Nam",
+        name: "Nguyễn Văn A",
+        gender: "Nam",
         date: "06/03/2024",
         email: "a@gmail.com",
         address: "Thanh Xuân, Hà Nội"  
@@ -10,8 +10,8 @@ function UserInfo() {
     <div>
         <h3>Thông tin cá nhân</h3>
         <ul>
-            <li>Họ và tên: {user.Name}</li>
-            <li>Giới tính: {user.sex}</li>
+            <li>Họ và tên: {user.name}</li>
+            <li>Giới tính: {user.gender}</li>
             <li>Ngày sinh: {user.date}</li>
             <li>Email: {user.email}</li>
             <li>Địa chỉ: {user.address}</li>
---
+++ ReactJS/src/SS13/src/App.tsx
@@ -0,0 +1,21 @@
+// import Exercises01 from "./Components/Exercises01"
+// import Exercises02 from "./Components/Exercises02"
+// import Exercises03 from "./Components/Exercises03"
+// import Parent_Comp from "./Components/BT4/Parent_Comp"
+// import ParentComponent from "./Components/BT5/Parentcomponent";
+import ListPost from "./Components/BT6/ListPost";
+function App() {
+
+  return (
+    <>
+      {/* <Exercises01 /> */}
+      {/* <Exercises02 /> */}
+      {/* <Exercises03 /> */}
+        {/* <Parent_Comp /> */}
+        {/* <ParentComponent /> */}
+        <ListPost />
+    </>
+  )
+}
+
+export default App
