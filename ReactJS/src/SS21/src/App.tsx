cebb189a5f90bcf803be22c4b40bdf4fb1cb733c
ThanhIT2006 <nguyenngocthanh15122006@gmail.com>
2025-09-03 22:44:55 +0700

    BT

---
+++ ReactJS/src/SS21/src/App.tsx
@@ -0,0 +1,20 @@
+ import 'bootstrap/dist/css/bootstrap.min.css';
+//  import BT1 from './Component/BT1';
+// import BT2 from './Component/BT2';
+// import BT3 from './Component/BT3';
+// import BT4 from './Component/BT4';
+import BT5 from './Component/BT5';
+function App() {
+
+  return (
+    <>
+      {/* <BT1 /> */}
+      {/* <BT2 /> */}
+      {/* <BT3 /> */}
+      {/* <BT4 /> */}
+      <BT5 />
+    </>
+  )
+}
+
+export default App
